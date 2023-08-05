87219c503d65809ff662bbadbc852c9a8855a1de
SAGAR SOLANKI <118007645+Sagarsolanki97@users.noreply.github.com>
2023-08-05 23:13:39 +0530

    check

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/item/:id' element={<Detail/>}></Route>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
